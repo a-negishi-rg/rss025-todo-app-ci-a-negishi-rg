cf3432b605f55acf4eabfb0b98fab6a8b17a223f
a-negishi <a-negishi@race-ss.co.jp>
2025-07-11 14:36:27 +0900

    RSS025_TRAINING_PJ-59 Vue.js＋LaravelのTODOリスト：登録画面作成＿TaskCreate.vue微修正

--- resources/js/pages/TaskCreate.vue
+++ resources/js/pages/TaskCreate.vue
@@ -5,20 +5,20 @@ import { ref } from 'vue';
 
 <template>
     <form>
-        <div class="max-w-xs mx-auto item-center my-2 mx-4">
-            <label class="block text-xs text-gray-600 font-medium pt-2">タイトル</label>
-            <input class="w-full border rounded-lg p-2 focus:outline-none focus:ring focus:ring-[var(--color-accent)] cursor-pointer">
+        <div class="max-w-xs mx-auto item-center">
+            <label class="block text-xs text-gray-600 font-medium mt-4">タイトル</label>
+            <input class="w-full border rounded-lg py-2 px-4 focus:outline-none focus:ring focus:ring-[var(--color-accent)] cursor-pointer">
         </div>
-        <div class="max-w-xs mx-auto item-center my-2 mx-4">
-            <label class="block text-xs text-gray-600 font-medium pt-2">内容</label>
-            <textarea class="w-full border rounded-lg p-2 focus:outline-none focus:ring focus:ring-[var(--color-accent)] cursor-pointer"></textarea>
+        <div class="max-w-xs mx-auto item-center">
+            <label class="block text-xs text-gray-600 font-medium mt-4">内容</label>
+            <textarea class="w-full border rounded-lg py-2 px-4 focus:outline-none focus:ring focus:ring-[var(--color-accent)] cursor-pointer"></textarea>
         </div>
-        <div class="max-w-xs mx-auto item-center my-2 mx-4">
-            <label class="block text-xs text-gray-600 font-medium pt-2">担当者</label>
-            <input class="w-full border rounded-lg p-2 focus:outline-none focus:ring focus:ring-[var(--color-accent)] cursor-pointer">
+        <div class="max-w-xs mx-auto item-center">
+            <label class="block text-xs text-gray-600 font-medium mt-4">担当者</label>
+            <input class="w-full border rounded-lg py-2 px-4 focus:outline-none focus:ring focus:ring-[var(--color-accent)] cursor-pointer">
         </div>        
         <div class="flex justify-center">
-            <button type="submit" class="w-xs text-white bg-[var(--color-accent)] border border-black rounded-lg p-[10px] m-3 cursor-pointer">
+            <button type="submit" class="w-xs text-white bg-[var(--color-accent)] border border-black rounded-lg p-[10px] mt-4 cursor-pointer">
                 追加
             </button>
         </div>    
