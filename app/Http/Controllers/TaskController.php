bf432448efaf5d5afa23a87bbc9b6c4cd09fda4e
a-negishi <a-negishi@race-ss.co.jp>
2025-07-17 11:41:50 +0900

    RSS025_TRAINING_PJ-61 Vue.js＋LaravelのTODOリスト：バリデーション作成＿新規作成

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,47 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
+use App\Http\Requests\taskRequest;
 use App\Models\Task;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class TaskController extends Controller
 {
     /**
      * RSS025_TRAINING_PJ-58 一覧画面表示
-     * 
-     * @return JsonResponse
      */
     public function index(): JsonResponse
     {
         try {
             $tasks = Task::get();
+
             return response()->json($tasks);
         } catch (Exception $e) {
             Log::error($e->getMessage());
+
             return response()->json($e);
         }
     }
 
     /**
      * RSS025_TRAINING_PJ-60 登録処理作成
-     * 
+     *
      * @return JsonResponse
      */
-    public function save(Request $request)
+    public function save(taskRequest $request)
     {
         DB::beginTransaction();
         try {
             $new_task = Task::saveNewTask($request);
             DB::commit();
+
             return response()->json($new_task, 200);
         } catch (Exception $e) {
             DB::rollback();
-            Log::error("タスクの追加に失敗しました。:" . $e->getMessage());
+            Log::error('タスクの追加に失敗しました。:'.$e->getMessage());
+
             return response()->json($e, 500);
         }
     }
--- app/Http/Requests/taskRequest.php
+++ app/Http/Requests/taskRequest.php
@@ -7,22 +7,36 @@ use Illuminate\Foundation\Http\FormRequest;
 class taskRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
+     * RSS025_TRAINING_PJ-61 バリデーション作成
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * バリデーションルール
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'title' => ['required', 'string', 'max:30'],
+            'content' => ['required', 'string'],
+            'person_in_charge' => ['required', 'string', 'max:10'],
+        ];
+    }
+
+    /**
+     * エラーメッセージ
+     */
+    public function attributes(): array
+    {
+        return [
+            'title' => 'タイトル', // 'title' フィールドを「タイトル」と表示
+            'content' => '内容', // 'content' フィールドを「内容」と表示
+            'person_in_charge' => '担当者', // 'person_in_charge' フィールドを「担当者」と表示
         ];
     }
 }
