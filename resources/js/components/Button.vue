5cc1d6fd0c4d229dc817930297d1feb71d08ba50
a-negishi <a-negishi@race-ss.co.jp>
2025-07-22 18:53:56 +0900

    RSS025_TRAINING_PJ-270 Vue.js＋LaravelのTODOリスト：ヘッダー作成＿Button.Vue微修正

--- resources/js/components/Button.vue
+++ resources/js/components/Button.vue
@@ -6,9 +6,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div>
-        <RouterLink :to=link class="float-left m-2 px-4 py-2 bg-white text-xs font-medium shadow-sm border border-[var(--color-input-border)] rounded-md hover:bg-gray-200 active:outline-offset-1 active:outline-2 active:outline-[var(--color-accent)]">
-            {{ name }}
-        </RouterLink>
-    </div>
+    <RouterLink :to=link class="float-left m-2 px-4 py-2 bg-white text-xs font-medium shadow-sm border border-[var(--color-input-border)] rounded-md hover:bg-gray-200 active:outline-offset-1 active:outline-2 active:outline-[var(--color-accent)]">
+        {{ name }}
+    </RouterLink>
 </template>
